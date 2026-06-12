2312a7a9b962f496e569e04adea20bb37e665bab
Parth <parthsata1709@gmail.com>
2026-06-12 15:40:27 +0530

    fix: remove script tag from index.html and update renderer template

--- .vercel/output/functions/__server.func/_chunks/renderer-template.mjs
+++ .vercel/output/functions/__server.func/_chunks/renderer-template.mjs
@@ -2,7 +2,7 @@ import { b as HTTPResponse } from "../_libs/h3.mjs";
 import "../_libs/rou3.mjs";
 import "../_libs/srvx.mjs";
 import "node:stream";
-const rendererTemplate = () => new HTTPResponse('<!doctype html>\r\n<html lang="en">\r\n  <head>\r\n    <meta charset="utf-8" />\r\n    <meta name="viewport" content="width=device-width, initial-scale=1.0" />\r\n    <title>PopX</title>\r\n  </head>\r\n  <body>\r\n    <script type="module" src="/src/main.tsx"><\/script>\r\n  </body>\r\n</html>\r\n', { headers: { "content-type": "text/html; charset=utf-8" } });
+const rendererTemplate = () => new HTTPResponse('<!doctype html>\r\n<html lang="en">\r\n  <head>\r\n    <meta charset="utf-8" />\r\n    <meta name="viewport" content="width=device-width, initial-scale=1.0" />\r\n    <title>PopX</title>\r\n  </head>\r\n  <body>\r\n  </body>\r\n</html>\r\n', { headers: { "content-type": "text/html; charset=utf-8" } });
 function renderIndexHTML(event) {
   return rendererTemplate(event.req);
 }
